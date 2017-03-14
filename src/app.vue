5787350d5305a409f318d67364c4074b4e5c3bec
dongne <neal.dong@oocl.com>
2017-03-14 17:19:56 +0800

    utilize vue and element-ui

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,17 @@
 <template>
 	<div>
+		<nav></nav>
+		<el-row>
+			<el-col :span="24">
+				<div class="grid-content bg-purpule-dark">
+					<el-button type="text" class="nav-btn" v-on:click="clickBtn">Research</el-button>
+					<el-button type="text" class="nav-btn">Work</el-button>
+					<el-button type="text" class="nav-btn">Students</el-button>
+					<el-button type="text" class="nav-btn">Paper</el-button>
+				</div>
+			</el-col>
+		</el-row>
+		<el-button>default button</el-button>
 		<div>
 			<router-link to="/about">about</router-link>
 		</div>
--- webpack.config.js
+++ webpack.config.js
@@ -10,77 +10,106 @@ module.exports = {
 		filename: 'build.js'
 	}, 
 	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				use: {
-					loader: "vue-loader",
-					options: {
-						loaders: {
-							css: ExtractTextPlugin.extract({
-							    use: ['css-loader', "postcss-loader"]
-							}),
-							stylus: ExtractTextPlugin.extract({
-							    use: ["css-loader", "postcss-loader", "stylus-loader"]
-							})
-						}
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: ["vue-style-loader", "css-loader"]
-			},
-			{
-				test: /\.styl$/,
-				use: ["vue-styke-loader", "css-loader", "stylus-loader"]
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				use: [{
-					loader: "url-loader",
-					options: {
-						limit: 10000,
-						name: 'img/[name].[hash:7].[ext]'
-					}
-				}]
-			}, 
-			{
-				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-				use: [{
-					loader: "url-loader",
-					options: {
-						limit: 10000,
-						name: 'fonts/[name].[hash:7].[ext]'
-					}
-				}]
-			},
-			{
-				test: /\.js$/,
-				use: "babel-loader",
-				include: [path.resolve(__dirname, 'src')]
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					use: ["css-loader", "postcss-loader"]
-				})
-			},
-			{
-				test: /\.styl$/,
-				use: ExtractTextPlugin.extract({
-					use: ["css-loader", "postcss-loader", "stylus-loader"]
-				})
-			}]
-	},
+	// 	rules: [
+	// 		{
+	// 			test: /\.vue$/,
+	// 			use: {
+	// 				loader: "vue-loader",
+	// 				// options: {
+	// 				// 	loaders: {
+	// 				// 		css: ExtractTextPlugin.extract({
+	// 				// 		    use: ['css-loader', "postcss-loader"]
+	// 				// 		}),
+	// 				// 		stylus: ExtractTextPlugin.extract({
+	// 				// 		    use: ["css-loader", "postcss-loader", "stylus-loader"]
+	// 				// 		})
+	// 				// 	}
+	// 				// }
+	// 			}
+	// 		},
+	// 		{
+	// 			test: /\.css$/,
+	// 			use: ["vue-style-loader", "css-loader"]
+	// 		},
+	// 		{
+	// 			test: /\.styl$/,
+	// 			use: ["vue-style-loader", "css-loader", "stylus-loader"]
+	// 		},
+	// 		{
+	// 			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+	// 			use: [{
+	// 				loader: "url-loader",
+	// 				options: {
+	// 					limit: 10000,
+	// 					name: 'img/[name].[hash:7].[ext]'
+	// 				}
+	// 			}]
+	// 		}, 
+	// 		{
+	// 			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+	// 			use: [{
+	// 				loader: "url-loader",
+	// 				options: {
+	// 					limit: 10000,
+	// 					name: 'fonts/[name].[hash:7].[ext]'
+	// 				}
+	// 			}]
+	// 		},
+	// 		{
+	// 			test: /\.js$/,
+	// 			use: "babel-loader",
+	// 			include: [path.resolve(__dirname, 'src')]
+	// 		},
+	// 		// {
+	// 		// 	test: /\.css$/,
+	// 		// 	use: ExtractTextPlugin.extract({
+	// 		// 		use: ["css-loader", "postcss-loader"]
+	// 		// 	})
+	// 		// },
+	// 		// {
+	// 		// 	test: /\.styl$/,
+	// 		// 	use: ExtractTextPlugin.extract({
+	// 		// 		use: ["css-loader", "postcss-loader", "stylus-loader"]
+	// 		// 	})
+	// 		// }
+	// 		]
+	// },
+	//   module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+    },
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': '"production"'
 		}),
 		new webpack.optimize.UglifyJsPlugin(),
-		new ExtractTextPlugin({
-			filename: "css/style.css"
-		}),
+		// new ExtractTextPlugin({
+		// 	filename: "css/style.css"
+		// }),
 		new HtmlWebpackPlugin({
 			filename: 'index.html',
 			template: 'index.tpl.html'
